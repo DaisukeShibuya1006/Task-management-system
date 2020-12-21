af5adb4ebfd38f65d39716bab493d7959d07d530
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-12-21 17:31:25 +0900

    ユーザーとタスクを紐付け、user_idにインデックスを貼りました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 15 }
-  validates :password_digest, length: { minimum: 8, maximum: 20 }
+    has_many :tasks, dependent: :destroy
+    has_secure_password
 end
