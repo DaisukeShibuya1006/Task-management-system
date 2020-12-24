3833f1f74ab9fe900cc56ef28395e7f7fd12c42c
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-12-24 14:21:57 +0900

    いくつかのファイルに改行を加えました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 15 }
-    validates :password_digest, length: { minimum: 8, maximum: 20 }
-    has_many :tasks, dependent: :destroy
-    has_secure_password
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :password_digest, length: { minimum: 8, maximum: 20 }
+
+  has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
+10000.times do |n|
+   User.create!(name: "testuser#{n+1}",
+               email: "testuser#{n+1}@hoge.jp",
+               password_digest: "testuser#{n+1}")
+end
 
-200.times do |n|
-   User.create(name: "testuser#{n+1}", email: "testuser#{n+1}@hoge.jp", password_digest: "testuser#{n+1}")
+100000.times do |n|
+   Task.create!(title: "testtitle#{n+1}",
+               text: "testtext#{n+1}",
+               status: rand(0..2),
+               priority: rand(0..2),
+               user_id: rand(1..10000))
 end
