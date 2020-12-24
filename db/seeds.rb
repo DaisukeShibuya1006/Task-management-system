b205e57e9626b41e163f62316c960b3be3c007f6
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-12-24 16:37:41 +0900

    生成する値を変更しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
-10000.times do |n|
-   User.create!(name: "testuser#{n+1}",
-               email: "testuser#{n+1}@hoge.jp",
-               password_digest: "testuser#{n+1}")
-end
-
-100000.times do |n|
+10.times do |n|
    Task.create!(title: "testtitle#{n+1}",
                text: "testtext#{n+1}",
                status: rand(0..2),
                priority: rand(0..2),
-               user_id: rand(1..10000))
+               user_id: 1)
 end
