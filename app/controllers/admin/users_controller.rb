854750abead8857b4167b7b9887110cfafa63d35
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2021-02-16 17:59:37 +0900

    fix: users_controller.rb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -33,7 +33,6 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       flash[:success] = 'ユーザーを登録しました。'
       redirect_to admin_user_url(@user)
     else
