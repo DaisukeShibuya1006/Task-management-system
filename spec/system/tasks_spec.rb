8340d0c62e714b69d3febd7063b79e0337cd8457
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2021-03-08 15:39:42 +0900

    Fix spec_comment

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,7 +6,7 @@ describe 'Task' do
       FactoryBot.create(:task, title: 'タイトルA', text: '詳細A')
     end
 
-    it 'descending the task creation date and time' do
+    it 'to display list of tasks' do
       visit tasks_path
       @tasks = Task.all.order(created_at: :desc)
     end
